6046d95969cfec6dfbd757dc68bc2bcddfb06303
thusiru <147712158+thusiru@users.noreply.github.com>
2026-05-04 10:21:09 +0530

    fix: fix errors found in eks module

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,17 +1,32 @@
+data "aws_caller_identity" "current" {}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.20.0"
 
   name               = "eks-microservices"
-  kubernetes_version = "1.35"
+  kubernetes_version = "1.33"
+
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  endpoint_public_access  = true
+  endpoint_private_access = true
+
+  enable_cluster_creator_admin_permissions = true
+  enable_irsa                              = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.public_subnets
-
-  endpoint_public_access                   = true
-  enable_cluster_creator_admin_permissions = true
-  enable_irsa                              = true
 
   eks_managed_node_groups = {
     app_nodes = {
@@ -22,6 +37,18 @@ module "eks" {
       min_size     = 1
       max_size     = 3
       desired_size = 2
+    }
+  }
+
+  access_entries = {
+    root_access = {
+      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      policy_associations = {
+        admin = {
+          policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = { type = "cluster" }
+        }
+      }
     }
   }
 }
